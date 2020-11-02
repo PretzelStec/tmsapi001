b885df2087459153ee24e0eafec03d1893375aca
jake stec <jakestec99@gmail.com>
2020-11-02 16:34:50 -0600

    CORS updated

--- server.js
+++ server.js
@@ -28,6 +28,16 @@ mongoose.connect(
 // use json formats
 app.use(express.json());
 
+// CORS
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+    if (req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+});
+
 // use routes
 app.get('/', (req, res, next) => {
     res.status(201).json({
