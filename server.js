274a94282cd9c727b017c2772bc65f9c0122a6ca
jake stec <jakestec99@gmail.com>
2020-11-02 16:47:01 -0600

    CORS

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(express.json());
 // CORS
 app.use((req, res, next)=>{
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+    res.header("Access-Control-Allow-Headers", "*");
     if (req.method === 'OPTIONS'){
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
